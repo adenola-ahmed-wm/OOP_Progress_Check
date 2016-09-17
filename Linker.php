d445b01e910d32367a2b7a3eb97c6a391a5266ab
Start Coding Student 2 <session2@START-Coding-MBP-09.local>
2016-09-16 18:11:17 -0700

    Everything so far

--- Linker.php
+++ Linker.php
@@ -1,4 +1,27 @@
 <?php
 
+class car {
+    var $model;
+
+     public $brand;
+     public $year;
+     private $vin_number;
+
+     function __construct($car_model) {
+       $this->car = $car_model;
+     }
+
+   function get_name(){
+     return $this->$name
+{}}
+   function set_name($new_car) {
+     if($this->car != "Nissan Altima") {
+       $this->car = strtoupper($new_name);
+     }
+     }
+
+
+
+
 
  ?>
--- index.php
+++ index.php
@@ -1,3 +1,25 @@
+<html>
+
+
+<?php include("Linker.php"); ?>
+
 <?php
 
+$Honda = new car("Honda Civic");
+echo "Stefan's full name: " . $stefan->get_name();
+
+$james = new employee("Johnny Toes");
+echo "--->" . $james->get_name();
+
+// echo "Tell me private stuff" . $stefan->$pin_number
+// $jimmy = new person();
+// $stefan->set_name("Stefan Fishook");
+// $jimmy->set_name('Jimmy Neutron');
+
+// echo "Stefan's full name: "; $stefan->get_name();
+// echo "Jimmy's full name: "; $jimmy->get_name();
+
  ?>
+
+
+</html>
