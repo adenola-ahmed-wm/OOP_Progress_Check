f212e3ff4614cc4b2152d7159554513821f61da6
Start Coding Student 2 <session2@START-Coding-MBP-09.local>
2016-09-22 17:08:06 -0700

    Made the files for the static task

--- Static.php
+++ Static.php
@@ -1,3 +1,15 @@
 <?php
+class person {
+  public $name;
+  public static $minsocialsecurity = 8;
 
+  public static function entersocial($social){
+    if(strlen($social) >= self::$minsocialsecurity){
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
+echo person::$minsocialsecurity;
  ?>
